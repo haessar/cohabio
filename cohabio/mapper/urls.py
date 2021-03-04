cf3d26588b4ce39a8ac7734692bb26ce8fc3ac6d
wahhi <wahhill@gmail.com>
2021-03-04 22:31:03 +0000

    feature - new "node map" for displaying all nodes and intersect for given workplaces

--- cohabio/mapper/urls.py
+++ cohabio/mapper/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'mapper'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index_node_map/', views.index_node_map, name='index_node_map'),
     path('search/', views.search, name='search'),
+    path('node_map/', views.node_map, name='node_map'),
     path('results_report/', views.results_report, name='results_report'),
     path('contact/', views.contact, name='contact'),
     path('acknowledgements/', views.acknowledgements, name='acknowledgements'),
