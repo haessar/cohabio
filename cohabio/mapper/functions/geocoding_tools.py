ae21629f2174486eb4e8d792af6242c389faac42
wahhi <wahhill@gmail.com>
2021-03-04 22:43:18 +0000

    better variable names

--- cohabio/mapper/functions/geocoding_tools.py
+++ cohabio/mapper/functions/geocoding_tools.py
@@ -22,9 +22,10 @@ class GeoLocator(GoogleV3):
     def __init__(self, logger):
         super(GeoLocator, self).__init__(api_key=self.api_key)
         self.logger = logger
-    def return_gps_from_place_names(self, listofplaces):
-        listofcoords = []
-        for item in listofplaces:
+
+    def return_gps_from_place_names(self, list_of_places):
+        list_of_coords = []
+        for item in list_of_places:
             while True:
                 try:
                     gps = self.geocode(item)
@@ -32,11 +33,12 @@ class GeoLocator(GoogleV3):
                 except (GeocoderTimedOut, GeocoderQuotaExceeded):
                     self.logger.warning('Failed to geocode. Sleeping')
                     time.sleep(5)
-            if len(listofplaces) == 1:
+            if len(list_of_places) == 1:
                 return gps
             else:
-                listofcoords.append(gps)
-        return listofcoords
+                list_of_coords.append(gps)
+        return list_of_coords
+
     def average_gps(self, gps1, gps2):
         """
         Calculates the mean latitude and longitude of two given gps coordinates.
@@ -47,5 +49,5 @@ class GeoLocator(GoogleV3):
         loc2 = self.geocode(gps2)
         gpsloc2 = [loc2.latitude, loc2.longitude]
         lat = np.mean([gpsloc1[0], gpsloc2[0]])
-        longid = np.mean([gpsloc1[1], gpsloc2[1]])
-        return [lat, longid]
+        lng = np.mean([gpsloc1[1], gpsloc2[1]])
+        return [lat, lng]
