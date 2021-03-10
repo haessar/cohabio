e70247ff4249764b5f4a8bc24f2043d469a4a7bf
wahhi <wahhill@gmail.com>
2021-03-10 19:32:33 +0000

    split UserInfo model into User and SearchResults models

--- cohabio/mapper/models.py
+++ cohabio/mapper/models.py
@@ -1,46 +1,20 @@
 from django.db import models
 
 
-class UserInfo(models.Model):
-    time_stamp = models.DateTimeField('date published', auto_now_add=True)
-    entries = models.IntegerField(default=0)
-    work1 = models.CharField(max_length=200, default="-")
-    work2 = models.CharField(max_length=200, default="-")
-    trans1 = models.CharField(max_length=200, default="-")
-    trans2 = models.CharField(max_length=200, default="-")
-    mcom1 = models.IntegerField(default=0)
-    mcom2 = models.IntegerField(default=0)
+class User(models.Model):
+    workplace = models.CharField(max_length=200)
+    transport = models.CharField(max_length=200)
+    max_commute = models.IntegerField()
+    elements = models.IntegerField()
+
+
+class SearchResults(models.Model):
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    you = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name='results_you')
+    them = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name='results_them')
     results = models.IntegerField(default=0)
 
-    def __str__(self):
-        return str(self.time_stamp)
-
-    def entry_count(self):
-        return self.entries
-
-    def location_1(self):
-        return self.work1
-
-    def location_2(self):
-        return self.work2
-
-    def transport_1(self):
-        return self.trans1
-
-    def transport_2(self):
-        return self.trans2
-
-    def max_commute_1(self):
-        return self.mcom1
-
-    def max_commute_2(self):
-        return self.mcom2
-
-    def rslts(self):
-        return self.results
-
     class Meta:
-        app_label = 'mapper'
         ordering = ['time_stamp']
 
 
@@ -55,8 +29,3 @@ class PlaceData(models.Model):
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        app_label = 'mapper'
-        verbose_name = 'Place Data'
-        verbose_name_plural = verbose_name
